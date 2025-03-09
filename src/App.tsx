19a027e2fa655aab3d8fa44dea353e215808e1f7
Josué Salazaku <josuesalazaku@gmail.com>
2025-03-09 23:52:55 +0100

    Add FetchPosts component to App for displaying posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import FetchPosts from "./components/fetchPosts";
 import FetchUsers from "./components/fetchUsers";
 
 function App() {
   return (
     <main>
       <FetchUsers />
+      <FetchPosts/ >
     </main>
   );
 }
